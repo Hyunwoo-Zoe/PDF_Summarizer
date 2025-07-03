7bed7e8f0d5f63456e816e8a85b7d76953c4e944
Hyunwoo Zoe <gkgkdi12@gmail.com>
2025-07-03 16:09:18 +0900

    Upload project to CacheController branch

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 
 from fastapi import FastAPI
-from app.controller import pdf_summary_controller, chat_summary_controller, cache_management_controller
+from app.controller import pdf_summary_controller, chat_summary_controller, cache_management_controller, vector_management_controller
 
 app = FastAPI(title="Multi-Summary API")
 app.include_router(pdf_summary_controller.router)
 app.include_router(chat_summary_controller.router)
 app.include_router(cache_management_controller.router)
-
+app.include_router(vector_management_controller.router)
